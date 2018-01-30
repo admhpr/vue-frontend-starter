307f1958749d9fa603113d3028af5dfae1481e28
adam <adam@adamharpur.com>
2018-01-29 18:55:17 -0600

    fix(configs)

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="home">
+    <h1>{{ msg }}</h1>
+    <p>{{ sass }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [],
+  name: "App",
+  data() {
+    return {
+      msg: "If these letters are green Webpack is working",
+      sass: "and Sass is working"
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+
+#home {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #449c75;;
+  margin-top: 60px;
+}
+
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-console.log("hello app");
+import Vue from "vue/dist/vue.esm.js";
+import App from "./App.vue";
+
+var vm = new Vue({
+  el: "#app",
+  components: {
+    App
+  },
+  template: '<App/>',
+  data: {
+  }
+});
